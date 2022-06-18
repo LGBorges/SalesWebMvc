4aba081c9016e616047473d3d6ed1ba441e50c7a
Leônidas Gomes Borges <leogb_27@hotmail.com>
2022-06-18 16:43:42 -0300

    Fixing CSS and others

--- SalesWebMvc/Migrations/20220618191040_Initial2.Designer.cs
+++ SalesWebMvc/Migrations/20220618191040_Initial2.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesWebMvc.Data;
 
 namespace SalesWebMvc.Migrations
 {
     [DbContext(typeof(SalesWebMvcContext))]
-    partial class SalesWebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20220618191040_Initial2")]
+    partial class Initial2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
