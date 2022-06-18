a622312b010b671f5a27377a6e1807c8128a41c0
Leônidas Gomes Borges <leogb_27@hotmail.com>
2022-06-18 00:46:58 -0300

    Validation

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -40,6 +40,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FinAll();
+                var viewmodel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewmodel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -109,6 +115,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FinAll();
+                var viewmodel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewmodel);
+            }
             if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
